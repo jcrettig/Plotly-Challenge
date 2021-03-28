5d52a284bfb294704c546954053a1a80a9a234b1
Chris Rettig <jcrettig@fastmail.fm>
2021-03-28 11:17:05 -0400

    bar and bubble charts

--- static/js/app.js
+++ static/js/app.js
@@ -10,6 +10,12 @@ d3.json("samples.json").then((data) => {
     //Grab id numbers from the names array
     var SubjectIDNo = data.names
 
+    //Grab id numbers from the names array
+    var samplesAll = new Array()
+    for (var i = 0; i < data.samples.length; i++) {
+        samplesAll[data.samples[i]["id"]] = data.samples[i];
+    }
+
     //Grab sample_values from the samples array 
     var sampleValues = new Array()
     for (var i = 0; i < data.samples.length; i++) {
@@ -66,6 +72,7 @@ d3.json("samples.json").then((data) => {
 
     // //console.logs to test value grabs   
     // console.log(SubjectIDNo)
+    console.log(samplesAll)
     // console.log(sampleValues)
     // console.log(otuIds)
     // console.log(otuLabels)
@@ -97,15 +104,16 @@ d3.json("samples.json").then((data) => {
     d3.selectAll("body").on("change", updatePage);
 
     function updatePage() {
-        
+
         //Assign the value of the dropdown menu to a variable
         var dataID = dropdownMenu.property("value")
-       
+
         //----------------------------------------------------
         //Create Variable for Charts and Table
         //----------------------------------------------------
 
         //Sample Set Variables
+        var sample = samplesAll[dataID]         //samples
         var sampleSet = sampleValues[dataID]    //sample_values       
         var otuIdSet = otuIds[dataID]           //otu_ids     
         var otuLabelSet = otuLabels[dataID]     //otu_labels
@@ -117,8 +125,10 @@ d3.json("samples.json").then((data) => {
         var locationDi = mdLocate[dataID]       //location
         var wfreqDi = mdFreq[dataID]            //wfreq
 
+
         //console.logs to test value grabs
         console.log(dataID)
+        console.log(sample)
         console.log(sampleSet)
         console.log(otuIdSet)
         console.log(otuLabelSet)
@@ -133,42 +143,91 @@ d3.json("samples.json").then((data) => {
         //----------------------------------------------------
         //Create a horizontal bar chart to display the top 10
         //OTUs found in that individual.
+        // * Use `sample_values` as the values for the bar chart.
+        // * Use `otu_ids` as the labels for the bar chart.
+        // * Use `otu_labels` as the hovertext for the chart.
         //----------------------------------------------------
-        
 
-    //----------------------------------------------------
+        // //Sort data by sample values                                !!!!!!!!!!
+        // sortedSamples = sample.sort((a, b) => b.sample_values - a.sample_values)
+        // // console.log(sortedSamples)
+
+        // // slice the first 10 objects for plotting                  !!!!!!!!!!
+        // slicedSamples = sortedSamples.slice(0,10)
+        // // console.log(slicedSamples)
+
+        // // reverse the array                                        !!!!!!!!!!!
+        // reversedSamples = slicedSamples.reverse()
+        // // console.log(reversedSamples)
+
+        // //Create trace for chart
+        // var trace1 = {
+        //  x: reversedSamples.map(sample => sample.sample_values),
+        //  y: reversedSamples.map(id => id.otu_ids),
+        //  text: reversedSamples.map(label => label.otu_labels),
+        //  name:"Samples",
+        //  type:"bar",
+        //  orientation:"h" 
+        // }
+
+        // // Create data for Chart
+        // var data = [trace1]
+
+        // // Create Layout for Chart
+        // var layout = {
+        //     title: "Top Ten OTUs"        
+        // }
+
+        // //Render the plot to the div tag
+        // Plotly.newPlot("bar", data, layout)
+
+        //----------------------------------------------------
+        //Create a bubble chart that displays each sample.
+        // * Use `otu_ids` for the x values.
+        // * Use `sample_values` for the y values.
+        // * Use `sample_values` for the marker size.
+        // * Use `otu_ids` for the marker colors.
+        // * Use `otu_labels` for the text values.
+        //----------------------------------------------------
+
+        var trace1 = {
+            x: otuIdSet,
+            y: sampleSet,
+            text: otuLabelSet,
+            mode: 'markers',
+            marker: {
+              size: sampleSet,
+              color: otuIdSet,
+            }
+          };
+          
+          var data = [trace1];
+          
+          var layout = {
+            title: 'Marker Size',
+            showlegend: false,
+            height: 500,
+            width: 750
+          };
+          
+          Plotly.newPlot('bubble', data, layout);
+
+
+
+
+
+
+
+
+
+        //----------------------------------------------------
     }  // end function updatePage(){
-//----------------------------------------------------
+    //----------------------------------------------------
 })  // end d3.json("samples.json").then((data) => {}
 //----------------------------------------------------
 
 
 
-// * Use `sample_values` as the values for the bar chart.
-
-// * Use `otu_ids` as the labels for the bar chart.
-
-// * Use `otu_labels` as the hovertext for the chart.
-
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-
-// * Use `sample_values` as the values for the bar chart.
-
-// * Use `otu_ids` as the labels for the bar chart.
-
-// * Use `otu_labels` as the hovertext for the chart.
-
-// 3. Create a bubble chart that displays each sample.
-
-// * Use `otu_ids` for the x values.
-
-// * Use `sample_values` for the y values.
-
-// * Use `sample_values` for the marker size.
-
-// * Use `otu_ids` for the marker colors.
-
-// * Use `otu_labels` for the text values.
 
 
 // 4. Display the sample metadata, i.e., an individual's demographic information.
